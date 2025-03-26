017fa87848264de9104fc3244890692e12f557c6
Pradum <satendarpradum@gmail.com>
2025-03-26 16:10:08 +0530

    Added add action button

--- frontend/app/zap/create/page.tsx
+++ frontend/app/zap/create/page.tsx
@@ -1,15 +1,40 @@
-import React, { useState } from 'react'
 
-const page = () => {
-    const[selectedTrigger,setSelectedTrigger]=useState("");
-    const [selectedActions,setSelectedActions]=useState([]);
-  return (
-    <div>
-        <div className='w-full h-screen bg-slate-200 flex-col justify-center'>
+"use client";
 
+import  Appbar  from "../../../component/AppBar";
+import { ZapCell } from "../../../component/ZapCell";
+import  LinkButton  from "../../../component/Button/LinkButton";
+import { PrimaryButton } from "../../../component/Button/PrimaryButton";
+import { useState } from "react";
+
+export default function() {
+    const [selectedTrigger, setSelectedTrigger] = useState("");
+    const [selectedActions, setSelectedActions] = useState<{
+        availableActionId: string;
+        availableActionName: string;
+    }[]>([]);
+
+    return <div>
+       
+        <div className="w-full min-h-screen bg-slate-200 flex flex-col justify-center">
+            <div className="flex justify-center w-full">
+                <ZapCell name={selectedTrigger ? selectedTrigger : "Trigger"} index={1} />
+            </div>
+            <div className="w-full pt-2 pb-2">
+                {selectedActions.map((action, index) => <div key={index} className="pt-2 flex justify-center"> <ZapCell name={action.availableActionName ? action.availableActionName : "Action"} index={2 + index} /> </div>)}
+            </div>
+            <div className="flex justify-center">
+                <div>
+                    <PrimaryButton onClick={() => {
+                        setSelectedActions(a => [...a, {
+                            availableActionId: "",
+                            availableActionName: ""
+                        }])
+                    }}><div className="text-2xl">
+                        +
+                    </div></PrimaryButton>
+                </div>
+            </div>
         </div>
     </div>
-  )
 }
-
-export default page
